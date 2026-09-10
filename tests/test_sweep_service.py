05a49c457bf08e40a61ba56fdee32d8367a6ddd3
apmcshahpurabhitoni-gif <apmcshahpurabhitoni@gmail.com>
2026-09-10 16:57:38 +0530

    Delete tests/test_sweep_service.py

--- tests/test_sweep_service.py
+++
@@ -1,4 +0,0 @@
-from strategies import discover_strategies
-
-def test_sweep_plugin_is_registered():
-    s=discover_strategies().get("sweep_v2"); assert s.manifest.account=="sweep_4h" and s.manifest.schedule=="canonical_sweep_schedule"
